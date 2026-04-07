9673ed3a23b5f67940e8753dd59dd1837b6afd51
Gustavo <gustavomelo.oliveira@hotmail.com>
2026-04-06 21:24:50 -0300

    feat: add investment crud functions

--- src/database/crud.py
+++ src/database/crud.py
@@ -5,7 +5,7 @@ logger = logging.getLogger(__name__)
 
 PATH_db = 'data/financas.db'
 
-def insert(date, description, category, type, value, bank):
+def insert_flow(date, description, category, type, value, bank):
     with sqlite3.connect(PATH_db) as conn:
         cursor = conn.cursor()
         cursor.execute('''
@@ -15,8 +15,7 @@ def insert(date, description, category, type, value, bank):
         conn.commit()
     logger.info(f"Gasto inserido: {description} - R${value}")    
 
-
-def balance():
+def balance_flow():
     with sqlite3.connect(PATH_db) as conn:
         cursor = conn.cursor()
         
@@ -26,13 +25,42 @@ def balance():
         cursor.execute("SELECT SUM(value) FROM flow WHERE type = ?", ('Expense',))
         expense = cursor.fetchone()[0] or 0
         
-        balance = income - expense
-        
+        balance = income - expense   
     return balance
 
-def delete(id):
+def delete_flow(id):
     with sqlite3.connect(PATH_db) as conn:
         cursor = conn.cursor()
         cursor.execute("DELETE FROM flow WHERE id = ?", (id,))
         conn.commit()
-        logger.info(f"Item deletado: id {id}")
+    logger.info(f"Item deletado: id {id}")
+
+def insert_investment(date, institution, investment, movement, value, asset_name):
+    with sqlite3.connect(PATH_db) as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            INSERT INTO investment (date, institution, investment, movement, value, asset_name)
+            VALUES (?, ?, ?, ?, ?, ?)
+        ''', (date, institution, investment, movement, value, asset_name))
+        conn.commit()
+    logger.info(f"Investimentos sugeridos: {investment} - R${value}")
+
+def delete_investment(id):
+    with sqlite3.connect(PATH_db) as conn:
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM investment WHERE id = ?", (id,))
+        conn.commit()
+    logger.info(f"Item deletado: id {id}")
+
+def select_investment():
+    with sqlite3.connect(PATH_db) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM investment  ")
+        return cursor.fetchall()
+
+
+
+
+
+    
+
