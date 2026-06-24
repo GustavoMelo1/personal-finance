100f1b60a260b36a78b714436bd304cb46a7b08a
Gustavo <gustavomelo.oliveira@hotmail.com>
2026-06-23 23:08:35 -0300

    feat: implement POST /expenses to insert flow via insert_flow

--- src/api/main.py
+++ src/api/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from src.database.crud import select_flow, select_investment, select_wishes
+from src.database.crud import select_flow, select_investment, select_wishes, insert_flow, insert_investment, insert_wishes
+
 
 app = FastAPI()
-
 class Flow(BaseModel):
     date: str
     description:str
@@ -22,7 +22,7 @@ def flow():
 
 @app.post("/expenses")
 def create_flow(expense: Flow):
-    pass
+    insert_flow(expense.date, expense.description, expense.category, expense.type, expense.value, expense.bank)
 
 @app.get("/investments")
 def investments():
